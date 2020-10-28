3e8820da67cb8e27b7f3b537dd23bc5ee2b48573
Jay Spears <j.spears.denver@gmail.com>
2020-10-27 20:11:04 -0600

    api add

--- static/js/logic-combined2.js
+++ static/js/logic-combined2.js
@@ -93,13 +93,13 @@ function createFeatures(sst, landfall) {
 function createMap(sst, landfall) {
 
     // Define satelitte and lightmap layers
-    var satelitte = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var satelitte = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicnlhbi1lYmVybGUiLCJhIjoiY2tmdTE5emdtMG50NjMwcXE1czJiZjAwNSJ9.FRk3sJ1kycMKiTvkB9eViQ", {
         maxZoom: 18,
         id: "satellite-streets-v9",
         accessToken: api_key
     });
 
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoicnlhbi1lYmVybGUiLCJhIjoiY2tmdTE5emdtMG50NjMwcXE1czJiZjAwNSJ9.FRk3sJ1kycMKiTvkB9eViQ", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "light-v10",
